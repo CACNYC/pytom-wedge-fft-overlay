6dcafb2be0df80afc74b941147f7b4e0db2cf517
Chris Caffalette <ccaffalette@gmail.com>
2026-05-05 12:27:49 -0400

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,10 +1,9 @@
-
 #!/usr/bin/env bash
 
 python pytom_wedge_fft_overlay.py \
   --tomogram /path/to/tomogram.mrc \
   --tilt-angles /path/to/angles.tlt \
-  --voxel-size-angstrom VOXEL-SIZE-ANGSTROM \
+  --voxel-size-angstrom VOXEL_SIZE_ANGSTROM \
   --outdir wedge_fft_check \
   --slab-y 7 \
   --also-negated
